89d42889aa5757118e20dd7f6fdfca57a57ed7d5
charlesollion <charles.ollion@gmail.com>
2024-04-12 14:43:09 +0200

    defaulting to detection model in main branch

--- app/src/main/java/org/surfrider/surfnet/detection/tracking/Tracker.kt
+++ app/src/main/java/org/surfrider/surfnet/detection/tracking/Tracker.kt
@@ -137,7 +137,7 @@ class Tracker(det: TrackedDetection, idx: Int, lctn: Location?) {
     companion object {
         private const val MAX_TIMESTAMP = 2000
         private const val MAX_ANIMATION_TIMESTAMP = 1000
-        private const val NUM_CONSECUTIVE_DET = 5
+        private const val NUM_CONSECUTIVE_DET = 6
         private const val COVARIANCE_SMOOTHING_FACTOR = 0.2F
     }
 }
